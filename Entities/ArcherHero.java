8e2558478e173df3693fb58bc948259548e660a5
Mayara Sampaio <mayarasampaio.dev@gmail.com>
2025-12-29 12:00:10 +0000

    add attack in ArcherHero and KnightHero

--- Entities/ArcherHero.java
+++ Entities/ArcherHero.java
@@ -1,16 +1,80 @@
 package Entities;
 
+import Items.ConsumableCombat;
 import Items.MainWeapon;
 
 public class ArcherHero extends Hero{
 
 
-    public ArcherHero(String name, int maxHp, int currentHp, int strength, int gold, HeroType heroType, int level, MainWeapon mainWeapon) {
-        super(name, maxHp, currentHp, strength, gold, heroType, level, mainWeapon);
+    public ArcherHero(String name, int maxHp, int currentHp, int strength, int gold, HeroType heroType, int level, MainWeapon mainWeapon, boolean specialUsed) {
+        super(name, maxHp, currentHp, strength, gold, heroType, level, mainWeapon,specialUsed);
     }
 
+    /**
+     * preencher
+     * @param targetNpc
+     * @return
+     */
     @Override
     public boolean attack(Npc targetNpc) {
-    return true;
+
+        specialUsed = false;
+
+        while (this.currentHp > 0 && targetNpc.getCurrentHp() > 0) {
+
+            //utiliza uma das opções do menu ataque
+            int option = attackMenu();
+            int damage = 0;
+
+            switch (option) {
+                case 1:
+                    damage = normalAttack();
+                    break;
+
+                case 2:
+                    if (specialUsed) {
+                        System.out.println("Não possui mais ataque consumível!");
+                        continue;
+                    }
+                    damage = specialAttack();
+                    specialUsed = true;
+                    break;
+
+                case 3:
+                    ConsumableCombat consumable = choiceConsumableCombate();
+                    if (consumable == null) continue;
+                    damage = consumableAttack(consumable);
+                    break;
+            }
+
+            //ArcherHero
+            targetNpc.setCurrentHp(targetNpc.getCurrentHp() - damage);
+            System.out.println("Você causou " + damage + " de dano!");
+
+            if (targetNpc.getCurrentHp()<=0) {
+                System.out.println("O vilão foi derrotado!");
+                level++;
+                strength++;
+                currentHp += 10;
+                gold += targetNpc.getGold();
+
+                specialUsed = false;
+                return true;
+            }
+
+            // NPC + 10% damage
+            int npcDamage = (int) Math.round(targetNpc.getStrength() + (targetNpc.getStrength() * 0.10));
+            currentHp -= npcDamage;
+            System.out.println(targetNpc.name + "causou " + npcDamage + " de dano!");
+
+            if (currentHp <= 0) {
+                System.out.println("Você foi derrotado!");
+                specialUsed = false;
+                return false;
+            }
+        }
+
+        specialUsed = false;
+        return false;
     }
 }
--- Entities/KnightHero.java
+++ Entities/KnightHero.java
@@ -1,5 +1,6 @@
 package Entities;
 
+import Items.ConsumableCombat;
 import Items.MainWeapon;
 
 import java.util.Scanner;
@@ -10,21 +11,74 @@ public class KnightHero extends Hero {
         super(name, maxHp, currentHp, strength, gold, heroType, level, mainWeapon,specialUsed);
     }
 
+    /**
+     * preencher
+     * @param targetNpc
+     * @return
+     */
     @Override
     public boolean attack(Npc targetNpc) {
-        Scanner input = new Scanner(System.in);
 
-        while(currentHp > 0 && targetNpc.currentHp > 0){
+        specialUsed = false;
 
-            //first atack: NPC
-            int npcDamage = (int) (targetNpc.getStrength() * 0.80);
+        while (this.currentHp > 0 && targetNpc.getCurrentHp() > 0) {
+
+            // NPC - 80% damage
+            int npcDamage = (int) Math.round((targetNpc.getStrength() * 0.80));
             currentHp -= npcDamage;
+            System.out.println(targetNpc.name + "causou " + npcDamage + " de dano!");
 
-            //Hero atack
-            if (currentHp > 0) return false;
+            if (currentHp <= 0) {
+                System.out.println("Você foi derrotado!");
+                specialUsed = false;
+                return false;
+            }
+
+
+            //utiliza uma das opções do menu ataque
+            int option = attackMenu();
+            int damage = 0;
+
+            switch (option) {
+                case 1:
+                    damage = normalAttack();
+                    break;
+
+                case 2:
+                    if (specialUsed) {
+                        System.out.println("Não possui mais ataque consumível!");
+                        continue;
+                    }
+                    damage = specialAttack();
+                    specialUsed = true;
+                    break;
+
+                case 3:
+                    ConsumableCombat consumable = choiceConsumableCombate();
+                    if (consumable == null) continue;
+                    damage = consumableAttack(consumable);
+                    break;
+            }
+
+            //KnightHero
+            targetNpc.setCurrentHp(targetNpc.getCurrentHp() - damage);
+            System.out.println("Você causou " + damage + " de dano!");
+
+            if (targetNpc.getCurrentHp()<=0) {
+                System.out.println("O vilão foi derrotado!");
+                level++;
+                strength++;
+                currentHp += 10;
+                gold += targetNpc.getGold();
+
+                specialUsed = false;
+                return true;
+            }
+
 
         }
-        return true;
 
+        specialUsed = false;
+        return false;
     }
 }
